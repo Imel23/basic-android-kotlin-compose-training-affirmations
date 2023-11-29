0fe0534ab48646997402db61175a29ed3dc82b4a
imadelbouazzaoui <imad.elbouazzaoui.ee@gmail.com>
2023-11-29 17:22:48 +0100

    Create the list

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -26,6 +26,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -67,7 +69,9 @@ Card(modifier = modifier) {
             painter = painterResource(id = affirmation.imageResourceId),
             contentDescription = stringResource(affirmation.stringResourceId),
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth().height(194.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(194.dp)
         )
         Text(
             text = stringResource(id = affirmation.stringResourceId),
@@ -78,6 +82,23 @@ Card(modifier = modifier) {
 }
 
 }
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier){
+    LazyColumn(modifier = modifier){
+        items(affirmationList){
+            affirmation ->
+            run {
+                AffirmationCard(
+                    affirmation = affirmation,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+
+    }
+}
+
 @Composable
 fun AffirmationsApp() {
 
